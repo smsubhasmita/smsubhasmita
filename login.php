caba54617fb63c806abf69ca867791967fa50ac7
smsubhasmita <35562344+smsubhasmita@users.noreply.github.com>
2022-08-26 17:56:41 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+ 
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+        <a href="index.php/MyController/index" class="logo mr-auto">
+            <img src="assets/img/obocwwb_logo.png" alt="" class="img-fluid">
+        </a> 
+        <a href="#" class="logo1 ">
+            <img src="assets/img/cm_pic.jpg" alt="" class="img-fluid"><br>
+            <img src="assets/img/cm_name_designation.png" alt="" class="img-fluid cm_name_designation">
+        </a> 
+    </div>
+    <div class="container d-flex align-items-center">
+        <nav class="nav-menu d-none d-lg-block mr-auto">
+        <ul>    
+            <li class="active"><a href="index2.php"><i class="fa fa-home" style="font-size: 20px;"></i> Home</a></li>
+            <li class="text-warning"><a href="#two"  class="text-warning">Instructions</a></li>                    
+        </ul>
+        </nav> 
+        <nav class="nav-menu d-none d-lg-block ml-auto">
+            <ul> 
+                <li><a href="#"  class="text-warning "><b>Login</b></a></li>
+                <li><a href="#"  class="text-warning"><b>Logout</b></a></li>  
+            </ul>
+        </nav> 
+    </div>
+</header>
+    <main id="main">
+        <section class="inner-page">
+            <?php include("regfrom.php"); ?>
+        </section>
+    </main>     
+
+
+ 
